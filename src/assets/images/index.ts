c66e744143c593c45c7aa60e008c73f1c4278e52
Ryan Barley <rbarley6@gatech.edu>
2023-02-10 19:56:13 -0500

    Commit assets and utils

---
+++ src/assets/images/index.ts
@@ -0,0 +1,10 @@
+export { default as gt } from "./gt.png";
+export { default as tw } from "./tw.png";
+export { default as mcla } from "./mcla.png";
+export { default as e451 } from "./8451.png";
+export { default as gaslight } from "./gaslight.png";
+export { default as rb } from "./rb.png";
+export { default as photonics } from "./photonics.png";
+export { default as cmp } from "./cmp.png";
+export { default as nbl } from "./nbl.png";
+export { default as latte } from "./latte.png";
