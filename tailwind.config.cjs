8e2210e91938df27ca145fe736377d6fcb66f40d
Ryan Barley <rmbarley@gmail.com>
2023-03-05 16:27:52 -0500

    Refactor to Astro Components. Unify style

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,21 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        640: '40rem',
+        1280: '80rem',
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+      },
+    },
   },
   plugins: [require('daisyui')],
   daisyui: {
